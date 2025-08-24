b80dfa9de4858dd76268ed82893ce139fd413e34
Kenean Dita <KeneanSufa@gmail.com>
2025-08-24 18:36:14 +0300

    Task model to define task attributes and serialization method

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from . import db
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    completed = db.Column(db.Boolean, default=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "completed": self.completed,
+        }
